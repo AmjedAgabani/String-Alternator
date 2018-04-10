7ca231ded59c5ae5efe5f6cde777fc4fe2b493e7
Amjed Agabani <amjedagabani@gmail.com>
2018-04-10 09:26:12 +0100

    Used JQuery to select some  HTML ID's

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
 $("#my-form").submit(function (event) {
     event.preventDefault();
-    var stringOne = document.getElementById('first-word').value;
-    var stringTwo = document.getElementById('second-word').value;
+    var stringOne = $('#first-word').value;
+    var stringTwo = $('#second-word').value;
     var combinedString = stringOne + stringTwo;
     $("#combined-string").text(combinedString);    
 });
-
-
-
